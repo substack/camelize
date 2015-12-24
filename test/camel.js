0863c7027d3ebb80b433b8e19af46ebf69b1a7d8
Apoorv Agrawal <apoorv.agrawal@outlook.com>
2015-12-24 16:10:03 +0530

    Added blacklisted keys

--- test/camel.js
+++ test/camel.js
@@ -2,45 +2,74 @@ var test = require('tape');
 var camelize = require('../');
 
 var obj = {
-    fee_fie_foe: 'fum',
-    beep_boop: [
-        { 'abc.xyz': 'mno' },
-        { 'foo-bar': 'baz' }
-    ]
+  fee_fie_foe: 'fum',
+  beep_boop: [{
+    'abc.xyz': 'mno'
+  }, {
+    'foo-bar': 'baz'
+  }]
 };
 
-test('camelize a nested object', function (t) {
-    t.plan(1);
-    var res = camelize(obj);
-    t.deepEqual(res, {
-        "feeFieFoe": "fum",
-        "beepBoop": [
-            { "abcXyz": "mno" },
-            { "fooBar": "baz" }
-        ]
-    });
+test('camelize a nested object', function(t) {
+  t.plan(1);
+  var res = camelize(obj);
+  t.deepEqual(res, {
+    'feeFieFoe': 'fum',
+    'beepBoop': [{
+      'abcXyz': 'mno'
+    }, {
+      'fooBar': 'baz'
+    }]
+  });
 });
 
-test('string', function (t) {
-    t.plan(1);
-    t.equal(camelize('one_two'), 'oneTwo');
+test('string', function(t) {
+  t.plan(1);
+  t.equal(camelize('one_two'), 'oneTwo');
 });
 
-test('date', function (t) {
-    t.plan(1);
-    var d = new Date();
-    t.equal(camelize(d), d);
+test('date', function(t) {
+  t.plan(1);
+  var d = new Date();
+  t.equal(camelize(d), d);
 });
 
-test('regex', function (t) {
-    t.plan(1);
-    var r = /1234/;
-    t.equal(camelize(r), r);
+test('regex', function(t) {
+  t.plan(1);
+  var r = /1234/;
+  t.equal(camelize(r), r);
 });
 
-test('only camelize strings that are the root value', function (t) {
-    t.plan(2);
-    t.equal(camelize('foo-bar'), 'fooBar');
-    var res = camelize({ 'foo-bar': 'baz-foo' });
-    t.deepEqual(res, { fooBar: 'baz-foo' });
+test('only camelize strings that are the root value', function(t) {
+  t.plan(2);
+  t.equal(camelize('foo-bar'), 'fooBar');
+  var res = camelize({
+    'foo-bar': 'baz-foo'
+  });
+  t.deepEqual(res, {
+    fooBar: 'baz-foo'
+  });
+});
+
+test('ignore keys if specified', function(t) {
+  t.plan(1);
+  var res = camelize({
+    'foo-bar': 'baz',
+    '_test': 'stuff',
+    'baz-qux': {
+      '_thing': 'some value'
+    }
+
+  }, {
+    ignore: ['_test', '_thing']
+  });
+
+  t.deepEqual(res, {
+    'fooBar': 'baz',
+    '_test': 'stuff',
+    'bazQux': {
+      '_thing': 'some value'
+    }
+  });
+
 });
