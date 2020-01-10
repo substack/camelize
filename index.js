a238fefb5a6fa3acd3825968a01e1b88f44784de
Dan Wetherald <dan.wetherald@gmail.com>
2020-01-09 21:48:10 -0600

    Add ability to blacklist keys with a new options object

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
-var camelCase = require('camelcase');
+var camelCase = require("camelcase");
 
-module.exports = function(obj) {
-    if (typeof obj === 'string') return camelCase(obj);
-    return walk(obj);
+var defaults = {
+  blacklist: []
 };
 
-function walk (obj) {
-    if (!obj || typeof obj !== 'object') return obj;
-    if (isDate(obj) || isRegex(obj)) return obj;
-    if (isArray(obj)) return map(obj, walk);
-    return reduce(objectKeys(obj), function (acc, key) {
-        var camel = camelCase(key);
-        acc[camel] = walk(obj[key]);
-        return acc;
-    }, {});
+module.exports = function(obj, options = {}) {
+  if (typeof obj === "string") return camelCase(obj);
+  return walk(obj, { ...defaults, ...options });
+};
+
+function walk(obj, options) {
+  if (!obj || typeof obj !== "object") return obj;
+  if (isDate(obj) || isRegex(obj)) return obj;
+  if (isArray(obj)) return map(obj, walk, options);
+  return reduce(
+    objectKeys(obj),
+    function(acc, key) {
+      var newKey = options.blacklist.includes(key) ? key : camelCase(key);
+      acc[newKey] = walk(obj[key], options);
+      return acc;
+    },
+    {},
+    options
+  );
 }
 
-var isArray = Array.isArray || function (obj) {
-    return Object.prototype.toString.call(obj) === '[object Array]';
+var isArray =
+  Array.isArray ||
+  function(obj) {
+    return Object.prototype.toString.call(obj) === "[object Array]";
+  };
+
+var isDate = function(obj) {
+  return Object.prototype.toString.call(obj) === "[object Date]";
 };
 
-var isDate = function (obj) {
-    return Object.prototype.toString.call(obj) === '[object Date]';
-};
-
-var isRegex = function (obj) {
-    return Object.prototype.toString.call(obj) === '[object RegExp]';
+var isRegex = function(obj) {
+  return Object.prototype.toString.call(obj) === "[object RegExp]";
 };
 
 var has = Object.prototype.hasOwnProperty;
-var objectKeys = Object.keys || function (obj) {
+var objectKeys =
+  Object.keys ||
+  function(obj) {
     var keys = [];
     for (var key in obj) {
-        if (has.call(obj, key)) keys.push(key);
+      if (has.call(obj, key)) keys.push(key);
     }
     return keys;
-};
+  };
 
-function map (xs, f) {
-    if (xs.map) return xs.map(f);
-    var res = [];
-    for (var i = 0; i < xs.length; i++) {
-        res.push(f(xs[i], i));
-    }
-    return res;
+function map(xs, f, options) {
+  if (xs.map) return xs.map((x, i) => f(x, options));
+  var res = [];
+  for (var i = 0; i < xs.length; i++) {
+    res.push(f(xs[i], i));
+  }
+  return res;
 }
 
-function reduce (xs, f, acc) {
-    if (xs.reduce) return xs.reduce(f, acc);
-    for (var i = 0; i < xs.length; i++) {
-        acc = f(acc, xs[i], i);
-    }
-    return acc;
+function reduce(xs, f, acc, options) {
+  if (xs.reduce) return xs.reduce((e, i) => f(e, i, options), acc);
+  for (var i = 0; i < xs.length; i++) {
+    acc = f(acc, xs[i], i);
+  }
+  return acc;
 }
--- test/camel.js
+++ test/camel.js
@@ -1,43 +1,59 @@
-var assert = require('assert');
-var camelize = require('../');
+var assert = require("assert");
+var camelize = require("../");
 
-var obj = {
-    fee_fie_foe: 'fum',
-    beep_boop: [
-        { 'abc.xyz': 'mno' },
-        { 'foo-bar': 'baz' }
-    ]
+var options = {
+  blacklist: ["foo_biz"]
 };
 
-describe('camelize2', function () {
-    it('nested object', function () {
-        var res = camelize(obj);
-        assert.deepEqual(res, {
-            'feeFieFoe': 'fum',
-            'beepBoop': [
-                { 'abcXyz': 'mno' },
-                { 'fooBar': 'baz' }
-            ]
-        });
-    });
+var obj = {
+  fee_fie_foe: "fum",
+  beep_boop: [
+    { "abc.xyz": "mno" },
+    { "foo-bar": "baz" },
+    {
+      biz_foo: {
+        foo_biz: "biz",
+        beep_boop: [{ "bar-foo": 100 }]
+      }
+    }
+  ]
+};
 
-    it('string', function () {
-        assert.equal(camelize('one_two'), 'oneTwo');
+describe("camelize2", function() {
+  it("nested object", function() {
+    var res = camelize(obj, options);
+    assert.deepEqual(res, {
+      feeFieFoe: "fum",
+      beepBoop: [
+        { abcXyz: "mno" },
+        { fooBar: "baz" },
+        {
+          bizFoo: {
+            foo_biz: "biz",
+            beepBoop: [{ barFoo: 100 }]
+          }
+        }
+      ]
     });
+  });
 
-    it('date', function () {
-        var d = new Date();
-        assert.equal(camelize(d), d);
-    });
+  it("string", function() {
+    assert.equal(camelize("one_two"), "oneTwo");
+  });
 
-    it('regex', function () {
-        var r = /1234/;
-        assert.equal(camelize(r), r);
-    });
+  it("date", function() {
+    var d = new Date();
+    assert.equal(camelize(d), d);
+  });
 
-    it('only camelize strings that are the root value', function () {
-        assert.equal(camelize('foo-bar'), 'fooBar');
-        var res = camelize({'foo-bar': 'baz-foo'});
-        assert.deepEqual(res, {fooBar: 'baz-foo'});
-    });
+  it("regex", function() {
+    var r = /1234/;
+    assert.equal(camelize(r), r);
+  });
+
+  it("only camelize strings that are the root value", function() {
+    assert.equal(camelize("foo-bar"), "fooBar");
+    var res = camelize({ "foo-bar": "baz-foo" });
+    assert.deepEqual(res, { fooBar: "baz-foo" });
+  });
 });
